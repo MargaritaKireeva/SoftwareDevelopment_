851966e5bcbad9999cd4b6827a5dab4bb1b20325
Margarita Kireeva <margaritakireeva@mail.ru>
2021-12-11 11:56:30 +0400

    Добавьте файлы проекта.

---
+++ CustomDict/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CustomDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomDictionary<string, string> dict = new CustomDictionary<string, string>();
+            dict.Add("key1", "value1");
+            dict.Add("key2", "value2");
+            dict.Add("key3", "value3");
+            foreach (var item in dict)
+                Console.WriteLine("{0} {1}", item.Key, item.Value);
+            dict.Remove("key2");
+            Console.WriteLine("After Remove key2:");
+            foreach (var item in dict)
+                Console.WriteLine("{0} {1}", item.Key, item.Value);
+            dict["key1"] = "value4";
+            Console.WriteLine("After dict[key1] = value4");
+            foreach (string key in dict.Keys)
+                Console.WriteLine("{0} {1}", key, dict[key]);
+
+        }
+    }
+}
